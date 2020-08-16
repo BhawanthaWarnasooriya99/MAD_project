58ddba6c5ebf68bbc34efa1fbc19a1d05801a8be
Sasanka5426 <sasanka5426@gmail.com>
2020-08-16 23:08:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madproject/addNewMemberAdmin.java
@@ -0,0 +1,14 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addNewMemberAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_member_admin);
+    }
+}
